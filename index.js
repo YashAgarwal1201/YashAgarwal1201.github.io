e19ed398955d78e5bf85d3dddc62f7f9986d11ad
Yash Agarwal <78301494+YashAgarwal1201@users.noreply.github.com>
2023-01-26 17:37:22 +0530

    commit 2 26-01-2022

--- index.js
+++ index.js
@@ -2,4 +2,7 @@
 document.addEventListener("DOMContentLoaded", () => {
 	console.log('i am ready')
 	//mainNav()
+	fetch("data.json")
+	.then(res => res.json())
+	.then(data => console.log(data))
 })
