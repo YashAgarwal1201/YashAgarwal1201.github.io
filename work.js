97b6e6e2b11a3eadc2a2adeecc8889d7a38dc131
Yash Agarwal <78301494+YashAgarwal1201@users.noreply.github.com>
2023-06-17 23:36:27 +0530

    commit 1 17-06-2023

--- work.js
+++ work.js
@@ -1,73 +1,79 @@
 var industry = {
-    1: {
-        heading: "December, 2022 - till now",
-        value: "UI Developer - Intern @ Techolution India",
-        links: ""
-    }
-}
+  1: {
+    heading: "December, 2022 - till now",
+    value: "UI Developer - Intern @ Techolution India",
+    links: "",
+  },
+};
 
 export var work = {
-    title: "work",
-    imagePath: "./images/Work.png",
-    content: {
-        industry: {
-            btnTitle: "Work Experience",
-            heading: "Work Experience",
-            subContent: industry
-        },
-        techolution: {
-            btnTitle: "Techolution",
-            heading: "Techolution",
-            subContent: "Projects I have been part of: \"One Stop Demo Shop\", \"HR policy dashboard\".\n Link to my Techolution github profile",
-            links: "https://github.com/yashAg-techolution"
-        },
-        designs: {
-            btnTitle: "Banana",
-            heading: "Banana",
-            subContent: "Image Backgrounds and some random designs using 'css' and 'w3.css'. Click the below button to check them out.\n",
-            links: "https://yashagarwal1201.github.io/Banana/"
-        },
-        glide: {
-            btnTitle: "Glide",
-            heading: "Glide",
-            subContent: "TvMovieDb - TV Shows & Movies Database app made using Google Sheets and Glide (a no-code platform).",
-            links: "https://legoyashx2.glideapp.io"
-        },
-        marigold: {
-            btnTitle: "Marigold",
-            heading: "Marigold",
-            subContent: "Some mini and random reactJS projects. Click the below button to check it out.\n",
-            links: "https://yashagarwal1201.github.io/Marigold/"
-        },
-        // blueberry: {
-        //     btnTitle: "Blueberry",
-        //     heading: "Blueberry",
-        //     subContent: "A mini speech recognition project using Javascript (and, jQuery). Click the below button to check it out.\n",
-        //     links: "https://agarwalyash.epizy.com/Blueberry/"
-        // },
-        wheat: {
-            btnTitle: "Wheat",
-            heading: "Wheat",
-            subContent: "A mini project to type, view and download simple html files. Click to check this out.\n",
-            links: "https://yashagarwal1201.github.io/Wheat/"
-        },
-        canva: {
-            btnTitle: "Canva",
-            heading: "Canva",
-            subContent: "Some designs on Canva.\n",
-            links: "https://legoyashx1.my.canva.site/"
-        },
-        lotus: {
-            btnTitle: "Lotus",
-            heading: "Lotus",
-            subContent: "Nothing special but my PHP based portfolio website.",
-            links: "https://agarwalyash.epizy.com"
-        },
-        extra: {
-            btnTitle: "Extras",
-            heading: "Extras",
-            subContent: "Nothing special but you may check the github profile for more.",
-            links: "https://github.com/YashAgarwal1201"
-        }
-    }
-}
+  title: "work",
+  imagePath: "./images/Work.png",
+  content: {
+    industry: {
+      btnTitle: "Work Experience",
+      heading: "Work Experience",
+      subContent: industry,
+    },
+    techolution: {
+      btnTitle: "Techolution",
+      heading: "Techolution",
+      subContent:
+        'Projects I have been part of: "One Stop Demo Shop", "HR policy dashboard", "Skills competancy dashboard", "Techo sales dashboard".\n Link to my Techolution github profile',
+      links: "https://github.com/yashAg-techolution",
+    },
+    designs: {
+      btnTitle: "Banana",
+      heading: "Banana",
+      subContent:
+        "Image Backgrounds and some random designs using 'css' and 'w3.css'. Click the below button to check them out.\n",
+      links: "https://yashagarwal1201.github.io/Banana/",
+    },
+    glide: {
+      btnTitle: "Glide",
+      heading: "Glide",
+      subContent:
+        "TvMovieDb - TV Shows & Movies Database app made using Google Sheets and Glide (a no-code platform).",
+      links: "https://legoyashx2.glideapp.io",
+    },
+    marigold: {
+      btnTitle: "Marigold",
+      heading: "Marigold",
+      subContent:
+        "Some mini and random reactJS projects. Click the below button to check it out.\n",
+      links: "https://yashagarwal1201.github.io/Marigold/",
+    },
+    // blueberry: {
+    //     btnTitle: "Blueberry",
+    //     heading: "Blueberry",
+    //     subContent: "A mini speech recognition project using Javascript (and, jQuery). Click the below button to check it out.\n",
+    //     links: "https://agarwalyash.epizy.com/Blueberry/"
+    // },
+    wheat: {
+      btnTitle: "Wheat",
+      heading: "Wheat",
+      subContent:
+        "A mini project to type, view and download simple html files. Click to check this out.\n",
+      links: "https://yashagarwal1201.github.io/Wheat/",
+    },
+    canva: {
+      btnTitle: "Canva",
+      heading: "Canva",
+      subContent: "Some designs on Canva.\n",
+      links: "https://legoyashx1.my.canva.site/",
+    },
+    lotus: {
+      btnTitle: "Lotus",
+      heading: "Lotus",
+      subContent: "Nothing special but my PHP based portfolio website.",
+      links: "https://agarwalyash.epizy.com",
+    },
+    extra: {
+      btnTitle: "Extras",
+      heading: "Extras",
+      subContent:
+        "Nothing special but you may check the github profile for more.",
+      links: "https://github.com/YashAgarwal1201",
+    },
+  },
+};
