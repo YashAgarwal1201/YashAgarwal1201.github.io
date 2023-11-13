4fa1c6424935d3a0c00dda8ddebfcf28b00c0402
YashAgarwal1201 <legoyashx@gmail.com>
2023-11-13 22:20:18 +0530

    chore: creating vite project

--- feedback.js
+++
@@ -1,8 +0,0 @@
-export var feedback = {
-    title: "feedback",
-    imagePath: "./images/Cont.png",
-    content: {
-        mailTo: 'legoyashx@hotmail.com',
-        bcc: 'legoyashx@yahoo.com'
-    }
-}
--- home.js
+++
@@ -1,6 +0,0 @@
-export var home = {
-    title: "yash agarwal",
-    imagePath: "./images/MyImage.png",
-    content: "Associate UI Developer",
-    subContent: "Techolution"
-}
