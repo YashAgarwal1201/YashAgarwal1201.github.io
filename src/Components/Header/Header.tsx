942884da34d7a038a15cd4eb88fc8bfc3f4f9c1e
YashAgarwal1201 <legoyashx@gmail.com>
2023-11-14 23:54:56 +0530

    feat: header component

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Button } from 'primereact/button'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='w-[75px] h-full flex flex-col items-center justify-center gap-y-1'>
+      {/* <Button /> */}
+      <Button icon='pi pi-home' className='h-16 w-16 text-white bg-transparent border-2 border-white' />
+      <Button icon='pi pi-user' className='h-16 w-16 text-white bg-transparent border-2 border-white' />
+      <Button icon='pi pi-briefcase' className='h-16 w-16 text-white bg-transparent border-2 border-white' />
+    </div>
   )
 }
 
