2f01347d559e6ebd8a181e67dbf4c6d1d97ae655
YashAgarwal1201 <legoyashx@gmail.com>
2023-11-15 23:56:48 +0530

    feat: code update

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import { useState } from 'react'
 import Header from './Components/Header/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedButton, setSelectedButton] = useState('home')
 
   return (
-    <div className='w-screen h-[100dvh] flex items-center bg-black'>
-      <Header />
+    <div className='w-screen h-[100dvh] flex flex-col-reverse md:flex-row items-center bg-black'>
+      <Header selectedButton={selectedButton} setSelectedButton={setSelectedButton} />
 
       <div className='h-full w-full flex justify-center items-center text-white border-2 rounded-md mx-1 my-2'>
         <p>Content</p>
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,15 +1,35 @@
 import React from 'react'
-import { Button } from 'primereact/button'
+import { Button } from "primereact/button";
 
-const Header = () => {
+const Header = (selectedButton: string, setSelectedButton: React.Dispatch<React.SetStateAction<string>>) => {
+  console.log(selectedButton)
   return (
-    <div className='w-[75px] h-full flex flex-col items-center justify-center gap-y-1'>
-      {/* <Button /> */}
-      <Button icon='pi pi-home' className='h-16 w-16 text-white bg-transparent border-2 border-white' />
-      <Button icon='pi pi-user' className='h-16 w-16 text-white bg-transparent border-2 border-white' />
-      <Button icon='pi pi-briefcase' className='h-16 w-16 text-white bg-transparent border-2 border-white' />
+    <div className="w-full md:w-[75px] h[75px] md:h-full flex flex-row md:flex-col items-center justify-center gap-y-0 md:gap-y-1 gap-x-1 md:gap-x-0">
+      <Button
+        title="home"
+        icon="pi pi-home"
+        className={`h-16 w-16 text-black bg-white border-2 border-transparent hover:border-white`}
+        onClick={() => setSelectedButton("home")}
+      />
+      <Button
+        title="about"
+        icon="pi pi-user"
+        className={`h-16 w-16 text-white bg-transparent border-2 border-transparent hover:border-white`}
+        onClick={() => setSelectedButton("about")}
+      />
+      {/* <Button
+        title="work"
+        icon="pi pi-briefcase"
+        className="h-16 w-16 text-white bg-transparent border-2 border-white"
+      /> */}
+      <Button
+        title="feedback"
+        icon="pi pi-comment"
+        className={`h-16 w-16 text-white bg-transparent border-2 border-transparent hover:border-white`}
+        onClick={() => setSelectedButton("feedback")}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
