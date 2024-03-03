09618904cec388bcafc5b1362b1b556ba06cc690
YashAgarwal1201 <legoyashx@gmail.com>
2024-03-03 21:31:03 +0530

    feat: scroll on key press

--- src/Components/Menu/MenuDialog.tsx
+++ src/Components/Menu/MenuDialog.tsx
@@ -2,50 +2,17 @@ import React from "react";
 
 import { Accordion, AccordionTab } from "primereact/accordion";
 import { Dialog } from "primereact/dialog";
-import { SelectButton } from "primereact/selectbutton";
+import { InputSwitch } from "primereact/inputswitch";
 
 import { useAppContext } from "../../Services/AppContext";
 import { KeyboardShortcuts } from "../KeyboardShortcuts/KeyboardShortcuts";
 import "./MenuDialog.scss";
+import { themes } from "../../Data/Data";
 
 type MenuDialogProps = {
   showMenuDialog: boolean;
   setShowMenuDialog: React.Dispatch<React.SetStateAction<boolean>>;
 };
-
-const themes = [
-  {
-    name: "Google",
-    value: "google",
-    colors: ["#FAFAFA", "#FBBC05", "#34A853", "#4285F4", "#EA4335"],
-  },
-  {
-    name: "Classic Night",
-    value: "night",
-    colors: ["#0a0a0a", "#171717", "#404040", "#a3a3a3", "#fafafa"],
-  },
-  {
-    name: "Classic Light",
-    value: "light",
-    colors: ["#fafafa", "#a3a3a3", "#404040", "#171717", "#0a0a0a"],
-  },
-  {
-    name: "Sea Fog",
-    value: "seafog",
-    colors: ["#7da5aa", "#B8CAD0", "#d5e1e4", "#eeeadd", "#404040"],
-  },
-  {
-    name: "Vintage",
-    value: "vintage",
-    colors: ["#f5e1b3", "#dbb070", "#5a888a", "#5f588a", "#b8556c"],
-  },
-  {
-    name: "Fall",
-    value: "fall",
-    colors: ["#A1A365", "#D4985D", "#FFEFC2", "#8C584A", "#333846"],
-  },
-];
-
 const MenuDialog = ({ showMenuDialog, setShowMenuDialog }: MenuDialogProps) => {
   const { state, dispatch, showToast } = useAppContext();
 
@@ -137,14 +104,9 @@ const MenuDialog = ({ showMenuDialog, setShowMenuDialog }: MenuDialogProps) => {
           >
             <div className="flex justify-between items-center">
               <span>Use easy mode?</span>
-              <SelectButton
-                value={state.easyMode}
+              <InputSwitch
+                checked={state.easyMode}
                 onChange={(e) => handleEasyModeChange(e.value)}
-                options={[
-                  { label: "Yes", value: true },
-                  { label: "No", value: false },
-                ]}
-                className="border-2 rounded-md text-sm"
               />
             </div>
           </AccordionTab>
