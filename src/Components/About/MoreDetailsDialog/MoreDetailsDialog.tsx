2031c1b7c9b9690981878622650a9f7c8faf3b68
yashAg-techolution <yash.agarwal@techolution.com>
2024-04-01 18:46:39 +0530

    fix: style fixes

--- src/Components/About/MoreDetailsDialog/MoreDetailsDialog.tsx
+++ src/Components/About/MoreDetailsDialog/MoreDetailsDialog.tsx
@@ -29,14 +29,14 @@ const MoreDetailsDialog = ({
         }}
         dismissableMask={true}
         draggable={false}
-        header={<div className="text-color1 font-heading font-normal">{state.modalContent?.header}</div>}
+        header={<div className="text-color1 lg:text-3xl font-heading font-normal">{state.modalContent?.header}</div>}
         // className="aboutDialog w-full md:w-[65%] h-full md:h-[80%] absolute bottom-0 md:bottom-auto"
         // position={window.innerWidth < 768 ? "bottom" : "center"}
         className={`aboutDialog ${
           state.easyMode ? "w-full md:w-1/2" : "w-full md:w-[85%] mdl:w-[75%] lg:w-[65%]"
         } h-full md:h-[80%] absolute bottom-0 md:bottom-auto`}
         position={
-          window.innerWidth < 768
+          window?.innerWidth < 768
             ? "bottom"
             : state.easyMode
             ? "right"
@@ -44,15 +44,15 @@ const MoreDetailsDialog = ({
         }
       >
         <div className="h-full p-2 md:p-4 flex flex-col gap-y-3 text-color5 bg-color2 rounded-md overflow-y-auto">
-          {!state.modalContent?.header.includes("Contact") &&
+          {!state.modalContent?.header?.includes("Contact") &&
             state.modalContent?.body?.map((values: any) => (
               <>
-                <h3 className="w-fit pb-1 text-base md:text-lg font-medium font-heading border-b-2 border-color4">
+                <h3 className="w-fit pb-1 text-lg md:text-xl lg:text-2xl font-medium font-subheading border-b-2 border-color4">
                   {values?.year}
                 </h3>
                 <div className="flex flex-col font-content">
                   <div className="ml-8 sm:ml-12 md:text-base flex flex-col gap-y-2">
-                    <p className="flex items-center gap-x-5">
+                    <p className="flex items-center gap-x-5 text-base lg:text-lg">
                       <span>{values?.description}</span>
                       {state.modalContent?.header.includes("Projects") && (
                         <Link
@@ -65,15 +65,15 @@ const MoreDetailsDialog = ({
                       )}
                     </p>
                     {values?.projects && (
-                      <ul className="ml-4">
+                      <ul className="ml-4 font-subheading  text-base lg:text-lg">
                         {values.projects.map((content: string, k: number) => (
                           <li key={k}>{content}</li>
                         ))}
                       </ul>
                     )}
                     {values?.certifications && (
-                      <ul className="ml-4">
-                        {values.certifications.map((content: string, k: number) => (
+                      <ul className="ml-4 font-subheading text-base lg:text-lg">
+                        {values.certifications?.map((content: string, k: number) => (
                           <li key={k}>{content}</li>
                         ))}
                       </ul>
--- src/Components/Feedback/FeedbackFormDialog/FeedbackFormDialog.tsx
+++ src/Components/Feedback/FeedbackFormDialog/FeedbackFormDialog.tsx
@@ -53,7 +53,7 @@ const FeedbackFormDialog = ({
         draggable={false}
         header={
           <>
-            <div className="text-color1 font-heading font-normal">Please fill out this form</div>
+            <div className="text-color1 lg:text-3xl font-heading font-normal">Please fill out this form</div>
           </>
         }
         className={`aboutDialog ${
@@ -73,38 +73,38 @@ const FeedbackFormDialog = ({
         >
           <div className="flex flex-col md:flex-row gap-x-3 gap-y-3">
             <div className="w-full md:w-1/2 flex flex-col gap-y-3">
-              <label className="text-lg font-heading">Your Email Address:</label>
+              <label className="text-lg md:text-xl lg:text-2xl font-subheading">Your Email Address:</label>
 
               <input
                 title="Email address"
-                className="h-10 px-4 bg-color3 text-color1 rounded-lg font-content"
+                className="h-10 px-4 bg-color3 text-color1 text-base md:text-lg rounded-lg font-content"
                 type="email"
                 name="Email"
                 required
               />
             </div>
             <div className="w-full md:w-1/2 flex flex-col gap-y-3">
-              <label className="text-lg font-heading">Your Name:</label>
+              <label className="text-lg md:text-xl lg:text-2xl font-subheading">Your Name:</label>
 
               <input
                 title="Name"
-                className="h-10 px-4 bg-color3 text-color1 rounded-md font-content"
+                className="h-10 px-4 bg-color3 text-color1 text-base md:text-lg rounded-md font-content"
                 type="text"
                 name="Name"
               />
             </div>
           </div>
           <div className="w-full flex flex-col gap-y-3">
-            <label className="text-lg font-heading">Your Message:</label>
+            <label className="text-lg md:text-xl lg:text-2xl font-subheading">Your Message:</label>
 
             <textarea
               title="You message"
-              className="h-40 p-4 bg-color3 text-color1 font-content rounded-lg resize-none"
+              className="h-40 p-4 bg-color3 text-color1 text-base md:text-lg font-content rounded-lg resize-none"
               name="Msg"
               required
             ></textarea>
           </div>
-          <div className="w-full h-12 flex flex-row-reverse justify-center items-center gap-x-4 font-content">
+          <div className="w-full h-10 md:h-12 lg:h-14 text-base md:text-lg lg:text-xl flex flex-row-reverse justify-center items-center gap-x-4 font-heading">
             <Button
               title="click to send"
               icon={<span className="material-symbols-rounded mr-2">send</span>}
