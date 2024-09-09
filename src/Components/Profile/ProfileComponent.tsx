251828c0006b58cca2b3dc3cc431be1c5c8a5fb7
YashAgarwal1201 <legoyashx@gmail.com>
2024-09-09 14:32:20 +0530

    feat: profile component

--- src/Components/Profile/ProfileComponent.tsx
+++ src/Components/Profile/ProfileComponent.tsx
@@ -1,88 +1,88 @@
-import React from "react";
-
 import { Button } from "primereact/button";
 import { Card } from "primereact/card";
-import ContactLinks from "../About/ContactLinks/ContactLinks";
+import { TabPanel, TabView } from "primereact/tabview";
+
 import { aboutInfo } from "../../Data/Data";
+import ContactLinks from "../About/ContactLinks/ContactLinks";
+import "./ProfileComponent.scss";
 
 const ProfileComponent = () => {
   const buttonStyles = "w-full bg-color2";
   return (
-    <div className="w-full h-full flex flex-col md:flex-col mdl:flex-row items-center gap-y-3 border overflow-auto">
-      <div className="w-full mdl:w-1/2 h-auto mdl:h-full border-2">
-        <div className="w-full h-full mdl:h-[40%] py-10 mdl:py-4 flex flex-col justify-center items-center gap-y-2 mdl:gap-y-3 lg:gap-y-4 border">
+    <div className="profile-component w-full h-full flex flex-col items-center gap-y-3 border overflow-auto">
+      <div className="w-full border-2">
+        <div className="w-full h-full mdl:h-[400px] py-10 mdl:py-4 flex flex-col justify-center items-center gap-y-2 mdl:gap-y-3 lg:gap-y-4 border">
           <div className="w-[80%] max-w-[150px] h-auto aspect-square border rounded-md"></div>
           <h1 className="font-heading text-xl mdl:text-2xl">Yash Agarwal</h1>
         </div>
-
-        <div className="hidden mdl:grid grid-cols-2 gap-4 w-full h-0 mdl:h-[60%] p-5">
-          <Button
-            title="work"
-            icon={<span className="material-symbols-rounded">work</span>}
-            className={buttonStyles}
-          />
-          <Button
-            title="education"
-            icon={<span className="material-symbols-rounded">school</span>}
-            className={buttonStyles}
-          />
-          <Button
-            title="other projects"
-            icon={
-              <span className="material-symbols-rounded">developer_guide</span>
-            }
-            className={buttonStyles}
-          />
-          <Button
-            title="contact"
-            icon={
-              <span className="material-symbols-rounded">contact_page</span>
-            }
-            className={buttonStyles}
-          />
-        </div>
-      </div>
-      <div className="w-full mdl:w-1/2 h-auto mdl:h-full flex flex-col gap-y-4 overflow-auto">
-        {/* <div className="w-full border p-4">
-          <h2>Work</h2>
-          <Card title="tit"></Card>
-        </div>
-
-        <div className="w-full border p-4">
-          <h2>education</h2>
-          <Card title="tit"></Card>
-        </div>
-
-        <div className="w-full border p-4">
-          <h2>other projects</h2>
-          <Card title="tit"></Card>
+        {/* <div className="w-full h-0 mdl:h-[60%] flex justify-center items-center">
+          <div className="hidden mdl:grid grid-cols-2 gap-4 max-w-[500px] w-full aspect-square p-5">
+            <Button
+              title="work"
+              icon={<span className="material-symbols-rounded">work</span>}
+              className={buttonStyles}
+            />
+            <Button
+              title="education"
+              icon={<span className="material-symbols-rounded">school</span>}
+              className={buttonStyles}
+            />
+            <Button
+              title="other projects"
+              icon={
+                <span className="material-symbols-rounded">
+                  developer_guide
+                </span>
+              }
+              className={buttonStyles}
+            />
+            <Button
+              title="contact"
+              icon={
+                <span className="material-symbols-rounded">contact_page</span>
+              }
+              className={buttonStyles}
+            />
+          </div>
         </div> */}
-        {aboutInfo
-          .filter(
-            (values) => !values.header?.toLowerCase()?.includes("contact")
-          )
-          ?.map((values, keys) => (
-            <div className="w-full border p-4 flex flex-col gap-2" key={keys}>
-              <h2>{values.header}</h2>
-              {values.content.map((value, key) => (
-                <Card title={value.year}>
-                  <p>{value.description}</p>{" "}
-                  <ul>
-                    {value.projects?.map((v, k) => (
-                      <li key={k}>{v}</li>
-                    ))}
-                  </ul>
-                </Card>
-              ))}
-            </div>
-          ))}
-
-        <div className="w-full border p-4">
-          <h2>Contact</h2>
-          <Card>
-            <ContactLinks content={aboutInfo[aboutInfo.length - 1].content} />
-          </Card>
-        </div>
+      </div>
+      <div className="w-full h-auto flex flex-col gap-y-4">
+        <TabView className="bg-transparent">
+          {aboutInfo
+            .filter(
+              (values) => !values.header?.toLowerCase()?.includes("contact")
+            )
+            ?.map((values, keys) => (
+              <TabPanel
+                header={values?.header}
+                className="w-full p-4 flex flex-col gap-2 bg-transparent"
+                contentClassName="bg-color2"
+                headerClassName="p-0 h-full bg-transparent text-color1 text-lg sm:text-xl mdl: text-2xl font-content font-normal"
+                key={keys}
+              >
+                {values?.content?.map((value, key) => (
+                  <Card title={value?.year} key={key} className="">
+                    <p>{value?.description}</p>{" "}
+                    <ul>
+                      {value?.projects?.map((v, k) => (
+                        <li key={k}>{v}</li>
+                      ))}
+                    </ul>
+                  </Card>
+                ))}
+              </TabPanel>
+            ))}
+          <TabPanel
+            header="abc"
+            className="w-full p-4 flex flex-col gap-2 bg-transparent"
+            contentClassName="bg-color2"
+            headerClassName="bg-transparent text-color1 text-lg sm:text-xl mdl: text-2xl font-heading font-medium"
+          >
+            <Card>
+              <ContactLinks content={aboutInfo[aboutInfo.length - 1].content} />
+            </Card>
+          </TabPanel>
+        </TabView>
       </div>
     </div>
   );
