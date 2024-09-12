82f6dcda538a415088613c6d722e9cabe1babbef
YashAgarwal1201 <legoyashx@gmail.com>
2024-09-12 22:38:16 +0530

    feat: new data structure for about section

--- src/Components/Profile/ProfileComponent.tsx
+++ src/Components/Profile/ProfileComponent.tsx
@@ -2,12 +2,91 @@ import { Button } from "primereact/button";
 import { Card } from "primereact/card";
 import { TabPanel, TabView } from "primereact/tabview";
 
-import { aboutInfo } from "../../Data/Data";
+import {
+  ABOUT_CONTACT,
+  ABOUT_EDUCATION,
+  ABOUT_ME,
+  ABOUT_OTHER_PROJECTS,
+  ABOUT_WORK,
+  aboutInfo,
+} from "../../Data/Data";
 import ContactLinks from "../About/ContactLinks/ContactLinks";
 import "./ProfileComponent.scss";
 
+const Work = () => {
+  return ABOUT_WORK?.map((val, key) => (
+    <Card key={key} title={val.year}>
+      {val.position && <p>{val.position}</p>}
+      {val.projects && (
+        <ul>
+          {val.projects.map((v, k) => (
+            <li key={k}>{v}</li>
+          ))}
+        </ul>
+      )}
+    </Card>
+  ));
+};
+
+const Education = () => {
+  return ABOUT_EDUCATION.map((val, key) => (
+    <Card key={key} title={val.year}>
+      {val.institution && <p>{val.institution}</p>}
+      {val.description && <p>{val.description}</p>}
+    </Card>
+  ));
+};
+
+const OtherProjects = () => {
+  return ABOUT_OTHER_PROJECTS.map((val, key) => (
+    <Card key={key} title={val.year}>
+      <div className="w-full ps-3 sm:ps-4 md:ps-6 mdl:ps-7 ld:ps-10 2xl:ps-14 flex gap-x-3 flex-wrap">
+        {val.projects.length > 0 &&
+          val.projects.map((v, k) => (
+            <a
+              href={v.posterLink}
+              target="_blank"
+              key={k}
+              title={v.title}
+              className="w-20 aspect-square p-2 sm:p-3 mdl:p-4 bg-color3 cursor-pointer rounded-full"
+            >
+              <img
+                src={v.posterLink + "/logo.svg"}
+                alt={v.title}
+                className="w-full h-full object-contain"
+              />
+            </a>
+          ))}
+      </div>
+    </Card>
+  ));
+};
+
+const Contact = () => {
+  return (
+    <Card>
+      <div className="w-full flex justify-center items-center gap-x-3 flex-wrap">
+        {ABOUT_CONTACT.map((value, key) => (
+          <a
+            key={key}
+            title={value.title}
+            href={value.type === "email" ? `mailto:${value.link}` : value.link}
+            target="_blank"
+            className={`w-20 aspect-square flex justify-center items-center p-2 sm:p-3 mdl:p-4 bg-color2 cursor-pointer rounded-full text-2xl text-color4`}
+          >
+            <span
+              className={`pi pi-${
+                value.type === "email" ? "envelope" : value.type
+              } text-3xl`}
+            ></span>
+          </a>
+        ))}
+      </div>
+    </Card>
+  );
+};
+
 const ProfileComponent = () => {
-  const buttonStyles = "w-full bg-color2";
   return (
     <div className="profile-component w-full h-full flex flex-col items-center gap-y-3 overflow-auto">
       <div className="w-full ">
@@ -57,7 +136,7 @@ const ProfileComponent = () => {
         </div> */}
       </div>
       <div className="w-full h-auto flex flex-col gap-y-4">
-        <TabView className="bg-transparent">
+        {/* <TabView className="bg-transparent">
           {aboutInfo
             ?.filter(
               (values) => !values?.header?.toLowerCase()?.includes("contact")
@@ -72,7 +151,7 @@ const ProfileComponent = () => {
               >
                 {values?.content?.map((value, key) => (
                   <Card title={value?.year} key={key} className="">
-                    <p>{value?.description}</p>{" "}
+                    {value?.description ? <p>{value?.description}</p> : ""}
                     <ul>
                       {value?.projects?.map((v, k) => (
                         <li key={k}>{v}</li>
@@ -93,6 +172,38 @@ const ProfileComponent = () => {
                 content={aboutInfo[aboutInfo.length - 1]?.content}
               />
             </Card>
+          </TabPanel>
+        </TabView> */}
+
+        <TabView className="bg-transparent">
+          {ABOUT_ME?.filter(
+            (values) => !values?.header?.toLowerCase()?.includes("contact")
+          )?.map((values, keys) => (
+            <TabPanel
+              header={values?.header}
+              className="w-full p-4 flex flex-col gap-2 bg-transparent"
+              contentClassName="bg-color2"
+              headerClassName="p-0 h-10 md:h-12 lg:h-14 bg-transparent text-color1 text-base sm:text-lg mdl:text-xl font-content font-normal"
+              key={keys}
+            >
+              {values.header.toLowerCase()?.includes("work") ? (
+                <Work />
+              ) : values.header.toLowerCase()?.includes("projects") ? (
+                <OtherProjects />
+              ) : values.header.toLowerCase()?.includes("education") ? (
+                <Education />
+              ) : (
+                ""
+              )}
+            </TabPanel>
+          ))}
+          <TabPanel
+            header={aboutInfo[aboutInfo.length - 1]?.header}
+            className="w-full p-4 flex flex-col gap-2 bg-transparent"
+            contentClassName="bg-color2"
+            headerClassName="p-0 h-10 md:h-12 lg:h-14 bg-transparent text-color1 text-lg sm:text-xl mdl: text-2xl font-content font-normal"
+          >
+            <Contact />
           </TabPanel>
         </TabView>
       </div>
